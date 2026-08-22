60c4352f60d0c755fd2a70f70d66ae97e3d4d4f7
praktiskberedskap <praktiskberedskap@protonmail.com>
2026-08-22 21:47:37 +0200

    diverse beredskap i menyen

--- src/config/nb/navData.json.ts
+++ src/config/nb/navData.json.ts
@@ -6,7 +6,7 @@ const allCategories = posts.map((post) => post.data.categories).flat();
 const countedCategories = countItems(allCategories);
 const processedCategories = sortByValue(countedCategories);
 
-const categoryOrder = ["vann-og-mat", "lys-og-varme", "mental-beredskap", "kommunikasjon", "diverse"];
+const categoryOrder = ["vann-og-mat", "lys-og-varme", "mental-beredskap", "kommunikasjon", "diverse-beredskap"];
 processedCategories.sort(([a], [b]) => categoryOrder.indexOf(a) - categoryOrder.indexOf(b));
 
 const sentenceCase = (str: string) =>
